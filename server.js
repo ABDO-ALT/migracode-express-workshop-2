3441b4db13701c89c80c9487434651c5583d32ee
ABDO-ALT <abdomohamad009639@gmail.com>
2020-02-19 20:18:21 +0100

    Add PUT Method toupdate booking

--- server.js
+++ server.js
@@ -43,6 +43,14 @@ app.delete("/bookings/:bookingId", function(req, res) {
     res.status(404).send("Its not exists!");
   }
 });
+app.put("/bookings/:bookingId", function(req, res) {
+  const bookingId = parseInt(req.params.bookingId);
+  if (utils.checkBookingExists(bookingId)) {
+    res.send(utils.editBooking(bookingId, req.body));
+  } else {
+    res.status(404).send("Its not exists!");
+  }
+});
 
 app.listen(3000, function() {
   console.log("Server is listening on port 3000. Ready to accept requests!");
